5dee4f90efb48be99cccf6f524d2dc10a5f6322c
Newton McCollum <nmccollu@citadel.edu>
2016-09-27 17:09:21 +0000

    Started Working on hangperson, NTD next invalid guess

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,8 +8,12 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses
+  
   def initialize(word)
-    @word = word
+    @word = word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
   def self.get_random_word
@@ -17,6 +21,21 @@ class HangpersonGame
     require 'net/http'
     uri = URI('http://watchout4snakes.com/wo4snakes/Random/RandomWord')
     Net::HTTP.post_form(uri ,{}).body
+  end
+  
+  def guess(char)
+    
+    char = char.downcase
+    if @word.include? char and !@guesses.include? char
+      @guesses << char
+    elsif @word.include? char and @guesses.include? char
+      return false
+    elsif !@word.include? char and !@wrong_guesses.include? char
+      @wrong_guesses << char
+    else
+      return false
+    end
+    return true
   end
 
 end
